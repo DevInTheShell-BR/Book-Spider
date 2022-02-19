559ed362f1990752651c3de5cc67b20d002b6b0a
Diego Passos <devintheshell@gmail.com>
2022-02-19 20:09:19 -0300

    custom pipeline to clean the data

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A simple Scrapy project to extract data from all 1000 books of books.toscrape.co
 
 The project contains one spider that sneaks through all categories pages in order to extract all book's data from all books in the site.
 
-We are interested in getting these fields:
+I'm interested in getting these fields:
 - Book Title
 - Book Price
 - Book Image URL
--- bookstoscrape/pipelines.py
+++ bookstoscrape/pipelines.py
@@ -10,4 +10,10 @@ from itemadapter import ItemAdapter
 
 class BookstoscrapePipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        fields = ['title', 'price', 'image_url', 'details_page_url']
+        for field in fields:
+            if adapter.get(field):
+                adapter[field] = adapter[field][0]
+
         return item
